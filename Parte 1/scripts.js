b122d2910f8d6c2766f52f234300833e23244a07
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-17 11:03:12 -0300

    parte2

--- Parte 1/scripts.js
+++ Parte 1/scripts.js
@@ -16,15 +16,21 @@ function listarItens(){
     let itens = JSON.parse(localStorage.getItem("produtos"));
     lista.innerHTML = "";
 
-    if (itens && itens.length > 0)
-        produtos = items;
+    if (itens && itens.length > 0){
+        produtos = itens;
         for (let i = 0; i < itens.length; i++){
-            lista.innerHTML += `<li class="list-group-item">${itens[i]}</li>`;
-     }
+            lista.innerHTML += `<li class="list-group-item d-flex">
+                <div class="w-50">${itens[i].nome}</div>
+                <div class="w-25">${itens[i].qtd}</div>
+                <div class="w-25">${itens[i].preco}</div>
+            </li>`;
+      }
+    }  
+  }  
 
 listarItens();     
     
-}
+
 
 
 
---
+++ eventos.js
@@ -0,0 +1,9 @@
+let notas = [
+    10,
+    9,
+    8.5,
+    9.8,
+];
+
+let total = notas[0] + notas[1] + notas[2] + notas[3];
+   alert(total / 4);
