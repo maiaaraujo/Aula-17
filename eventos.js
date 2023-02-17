9efcddadccc560924c3c55355b15e1732870f0d1
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-17 11:29:25 -0300

    media

--- eventos.js
+++ eventos.js
@@ -1,9 +1,23 @@
-let notas = [
-    10,
-    9,
-    8.5,
-    9.8,
-];
+function calcularMedia(notas) {
+    let total = notas[0] + notas[1] + notas[2] + notas[3];
 
-let total = notas[0] + notas[1] + notas[2] + notas[3];
-   alert(total / 4);
+    let media = total/4;
+
+    return media;
+}
+
+let mediaG = calcularMedia([10, 9.5, 9, 9.8]);
+alert("Media do Gleidson" + mediaG);
+
+let mediaA = calcularMedia([9, 7, 6.8, 9]);
+alert("Media do Ale" + mediaA);
+
+// let notas = [
+//     10,
+//     9,
+//     8.5,
+//     9.8,
+// ];
+
+// let total = notas[0] + notas[1] + notas[2] + notas[3];
+//     alert(total/4);
